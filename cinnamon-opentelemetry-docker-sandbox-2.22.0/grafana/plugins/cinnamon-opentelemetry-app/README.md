d407e379b146f6d45fcaebab01f4245432496217
Janik Dotzel <janikdotzel96@gmail.com>
2025-12-10 15:10:12 +0100

    add OpenTelemetry Developer Sandbox

---
+++ cinnamon-opentelemetry-docker-sandbox-2.22.0/grafana/plugins/cinnamon-opentelemetry-app/README.md
@@ -0,0 +1,5 @@
+Dashboards for Akka Insights, using Cinnamon with a Prometheus (via OpenTelemetry) data source.
+
+See the [Cinnamon documentation][docs] for installing and configuring telemetry.
+
+[docs]: https://developer.lightbend.com/docs/cinnamon/latest/home.html
---
+++ cinnamon-opentelemetry-docker-sandbox-2.22.0/grafana/plugins/cinnamon-opentelemetry-app/module.js
@@ -0,0 +1,15 @@
+'use strict';
+
+System.register(['./components/config'], function (_export, _context) {
+    "use strict";
+
+    var CinnamonOpenTelemetryAppConfigCtrl;
+    return {
+        setters: [function (_componentsConfig) {
+            CinnamonOpenTelemetryAppConfigCtrl = _componentsConfig.CinnamonOpenTelemetryAppConfigCtrl;
+        }],
+        execute: function () {
+            _export('ConfigCtrl', CinnamonOpenTelemetryAppConfigCtrl);
+        }
+    };
+});
